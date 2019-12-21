4aea5b8908e8bf3bf28093ddd0723f477439e9df
Nathan Fielder <48207930+nfielder@users.noreply.github.com>
2019-12-21 14:42:19 +0000

    Added logic for handling hard allocated values

--- secret-santa-allocator.py
+++ secret-santa-allocator.py
@@ -9,10 +9,9 @@ def create_dictionary(names):
 
 
 def allocate(d, names):
-    d["Julia"] = "Megan"
-    allocated = ["Megan"]
+    allocated = get_hard_allocation(d)
     for key in d.keys():
-        if key == "Julia":
+        if d[key] != None:
             continue
         available = names.copy()
         available.remove(key)
@@ -28,9 +27,18 @@ def allocate(d, names):
     return d
 
 
+def get_hard_allocation(d):
+    arr = []
+    for key in d.keys():
+        if d[key] != None:
+            arr.append(d[key])
+    return arr
+
+
 def main():
     names = ["Nathan", "Justin", "Julia", "Finlay", "Megan"]
     unallocated_dict = create_dictionary(names)
+    unallocated_dict["Julia"] = "Megan"
     allocated_dict = allocate(unallocated_dict, names)
 
     option = ""
@@ -38,6 +46,10 @@ def main():
         option = input("Enter your name or (Q)uit: ")
         if option in allocated_dict.keys():
             print("\nYour allocated person is:", allocated_dict[option], "\n")
+        elif option.upper() == "Q":
+            continue
+        else:
+            print("\nThat name is not in the allocation. Please check your spelling.\n")
 
 
 if __name__ == "__main__":
